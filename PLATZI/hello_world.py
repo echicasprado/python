9daeda332d11a61f23e6d2f7a653331cd1b0f66d
echicasprado <everchicasp@outlook.com>
2023-01-10 11:03:39 -0600

    use input and concat variables to print messages

---
+++ PLATZI/hello_world.py
@@ -0,0 +1,13 @@
+print("Hellow world!")
+
+name = input("What's your name?\n")
+
+print("Hello ",name)
+
+print("USO DE f y format, para concatenar variables a mensajes")
+
+message1 = "Hola mundo!"
+message2 = input("Ingresa tu nombre")
+
+print ("Mensaje1 -> {0}".format(message1))
+print (f"Bienvenido {message2}")
