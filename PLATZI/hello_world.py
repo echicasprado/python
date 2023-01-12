845f727ddd2cd54f0daae6c67b0c18719efc92a8
echicasprado <everchicasp@outlook.com>
2023-01-12 15:56:12 -0600

    more types

--- PLATZI/hello_world.py
+++ PLATZI/hello_world.py
@@ -7,7 +7,6 @@ print("Hello ",name)
 print("USO DE f y format, para concatenar variables a mensajes")
 
 message1 = "Hola mundo!"
-message2 = input("Ingresa tu nombre")
 
 print ("Mensaje1 -> {0}".format(message1))
 print (f"Bienvenido {message2}")
@@ -15,9 +14,14 @@ print (f"Bienvenido {message2}")
 int1 = 4
 float1 = 5/3
 bool1 = True
+list1 = [1,2,3,4,5]
+tuple1 = (6,7,8,9,0)
+dictionary1 = {'key':'value','key2':'value2'}
 
 print("tipos de datos que se pueden usar en python")
 print(f"->string ejemplo de tipo de dato: {type(message1)}")
 print(f"->integer ejemplo de tipo de dato: {type(int1)}")
 print(f"->floar ejemplo de tipo de dato: {type(float1)}")
 print(f"->boolean ejemplo de tipo de dato: {type(bool1)}")
+print(f"->list ejemplo de tipo de dato: {type(list1)}")
+print(f"->tuple ejemplo de tipo de dato: {type(tuple1)}")
