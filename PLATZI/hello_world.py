91244a1fb73954c2fba7e16fbe9912961e235eaa
echicasprado <everchicasp@outlook.com>
2023-01-12 15:53:23 -0600

    types

--- PLATZI/hello_world.py
+++ PLATZI/hello_world.py
@@ -11,3 +11,13 @@ message2 = input("Ingresa tu nombre")
 
 print ("Mensaje1 -> {0}".format(message1))
 print (f"Bienvenido {message2}")
+
+int1 = 4
+float1 = 5/3
+bool1 = True
+
+print("tipos de datos que se pueden usar en python")
+print(f"->string ejemplo de tipo de dato: {type(message1)}")
+print(f"->integer ejemplo de tipo de dato: {type(int1)}")
+print(f"->floar ejemplo de tipo de dato: {type(float1)}")
+print(f"->boolean ejemplo de tipo de dato: {type(bool1)}")
